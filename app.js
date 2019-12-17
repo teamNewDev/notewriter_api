566aea2032319938085a0bc983906fdbb3e03df9
Ebenezer Don <ebenezerdonu@gmail.com>
2019-12-17 05:59:48 +0100

    chore(setup): set up initial project
    - add initial dependencies
    - set up express server
    - add start and build scripts
    
    [finishes #1]

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { userRoutes } from './app';
+
+dotenv.config();
+
+const app = express();
+
+app.enable('trust proxy');
+
+// Normal express config defaults
+app.use(require('morgan')('dev'));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api/v1/users', userRoutes);
+
+export default app;
